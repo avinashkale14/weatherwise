f111134c244a49508cc62b588a4ed1af8f5784d7
avinashkale14 <avinashkale8263@gmail.com>
2026-09-20 14:02:36 +0530

    Fix weather route latitude handling

--- assets/js/route.js
+++ assets/js/route.js
@@ -1,41 +1,52 @@
-
 'use strict';
 
-import {updateWeather, error404} from "./app.js";
-const defaultLocation = "#/weather?lat=7.7162&lon=110.3354"; //selman
+import { updateWeather, error404 } from "./app.js";
 
-const currentLocation = function(){
-    window.navigator.geolocation.getCurrentPosition(res => {
-        const {latitude, longitude} = res.coords;
+const defaultLocation = "#/weather?lat=7.7162&lon=110.3354";
 
-        updateWeather(`lat=${latitude}`, `lon=${longitude}`);
-    }, err =>{
-        window.location.hash=defaultLocation;
-    });
-}
+const currentLocation = function () {
+    window.navigator.geolocation.getCurrentPosition(
+        (res) => {
+            const { latitude, longitude } = res.coords;
 
-const searchedLocation = query => updateWeather(... query.split("&")); //update lat lon location
+            updateWeather(latitude, longitude);
+        },
+        () => {
+            window.location.hash = defaultLocation;
+        }
+    );
+};
+
+const searchedLocation = (query) => {
+    const params = new URLSearchParams(query);
+
+    const lat = params.get("lat");
+    const lon = params.get("lon");
+
+    updateWeather(lat, lon);
+};
 
 const routes = new Map([
     ["/current-location", currentLocation],
     ["/weather", searchedLocation]
 ]);
 
-const checkHash = function (){
+const checkHash = function () {
     const requestURL = window.location.hash.slice(1);
 
-    const [route, query] = requestURL.includes ? requestURL.split("?") : [requestURL]
+    const [route, query] = requestURL.includes("?")
+        ? requestURL.split("?")
+        : [requestURL];
 
     routes.get(route) ? routes.get(route)(query) : error404();
-}
+};
 
 window.addEventListener("hashchange", checkHash);
 
-window.addEventListener("load", function(){
-    if(!window.location.hash){
+window.addEventListener("load", function () {
+    if (!window.location.hash) {
         window.location.hash = "#/current-location";
-    }
-    else{
+    } else {
         checkHash();
     }
 });
